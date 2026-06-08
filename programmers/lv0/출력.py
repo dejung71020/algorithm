9623b186cf485c2bed4ac5667bb59e2f80e4f82c
dejung71020@gmail.com <dejung71020@gmail.com>
2026-06-08 20:34:04 +0900

    feat: 출력 및 readme 갱신

---
+++ programmers/lv0/출력.py
@@ -0,0 +1,35 @@
+'''
+문제 설명
+주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+출력 예시
+Spring is beginning
+13
+310
+
+빈칸채우기 문제입니다.
+string_msg = 
+
+int_val = 
+
+string_val = 
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
+'''
+
+string_msg = "Spring is beginning"
+
+int_val = 3
+
+string_val = "3" 
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
+
+'''
+너무 쉬워서 다른 문제 풀이를 보니
+int_val = len(string_msg.split()) 으로 우연의 일치를 이용하여 푼 문제를 제외하고는 정답은 일치했다.
+'''
